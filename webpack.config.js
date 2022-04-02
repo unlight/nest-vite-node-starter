12d5e4d7bc5d388ce01f7859ae7eeba7e858b7f1
Roman_Vasilev <s91630277@gmail.com>
2022-04-02 23:37:27 +0400

    build: Remove unnecessary plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,39 @@
+const { envBool } = require('env-bool');
+
+const hasWatchFlag =
+    process.argv[1]?.endsWith('nest.js') && process.argv.includes('--watch');
+
 /**
  * @param  { import('webpack').Configuration } config
  * @param  { import('webpack') } webpack
  */
 module.exports = (config, webpack) => {
-    config.devtool = 'source-map';
-    config.optimization.nodeEnv = 'production';
-    config.optimization.moduleIds = 'named';
-    config.optimization.chunkIds = 'named';
+    const isWatch = config.watch || hasWatchFlag;
+    const disableTsCheck = envBool(process.env.WORKSPACE_NO_TS_CHECK);
+
+    Object.assign(config, {
+        devtool: 'source-map',
+        mode: 'production',
+    });
+
+    Object.assign(config.optimization, {
+        nodeEnv: 'production',
+        minimize: false,
+        moduleIds: 'named',
+        chunkIds: 'named',
+    });
+
+    Object.assign(config.output, {
+        iife: false,
+    });
+
     config.plugins = config.plugins.filter(p => {
         const pluginName = p?.constructor?.name;
-        if (
-            config.watch &&
-            pluginName === 'ForkTsCheckerWebpackPlugin' &&
-            process.env.WORKSPACE_NO_TS_CHECK
-        ) {
+        if (isWatch && pluginName === 'ForkTsCheckerWebpackPlugin' && disableTsCheck) {
             return false;
         }
         return true;
     });
-    config.module.rules.unshift({
-        test: require.resolve('./src/main.ts'),
-        loader: 'condition-loader',
-        options: {
-            PROD: config.optimization.nodeEnv === 'production',
-        },
-    });
-
-    config.output.iife = false;
 
     return config;
 };
