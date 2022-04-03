7aa328df3ea4f2fc09571c60509ff6e4430cbcbd
Roman_Vasilev <s91630277@gmail.com>
2022-04-03 16:51:22 +0400

    build: Externals

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,36 @@
-import { defineConfig } from 'vite';
+import { ConfigEnv, defineConfig, loadEnv } from 'vite';
 import { VitePluginNode } from 'vite-plugin-node';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-    plugins: [
-        ...VitePluginNode({
-            adapter: 'nest',
-            appPath: './src/main.ts',
-            tsCompiler: 'swc',
-        }),
-    ],
+export default defineConfig(({ command, mode }: ConfigEnv) => {
+    return {
+        build: {
+            target: 'es2020',
+        },
+        optimizeDeps: {
+            // Vite does not work well with optionnal dependencies, mark them as ignored for now
+            exclude: [
+                '@nestjs/platform-socket.io',
+                '@nestjs/websockets',
+                '@nestjs/microservices',
+                'amqp-connection-manager',
+                'amqplib',
+                'nats',
+                '@grpc/proto-loader',
+                '@grpc/grpc-js',
+                'redis',
+                'kafkajs',
+                'mqtt',
+                'cache-manager',
+            ],
+        },
+        plugins: [
+            tsconfigPaths(),
+            ...VitePluginNode({
+                adapter: 'nest',
+                appPath: './src/main.ts',
+                tsCompiler: 'swc',
+            }),
+        ],
+    };
 });
